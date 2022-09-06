28549ea0f51bd57721ebca59de2583e83d7386d6
TheLDB <ldb@erikboles.com>
2022-09-05 21:01:19 -0600

    fix: removed println macro from eth_message

--- server/src/utils/eth_message.rs
+++ server/src/utils/eth_message.rs
@@ -18,7 +18,6 @@ use web3::signing::{keccak256};
 pub fn eth_message(address: &String, nonce: &String) -> [u8; 32] {
     let message = format!("Thanks for using Monatize. To authenticate, please sign this message.\nAddress: {}\nNonce: {}\n\nTOS: https://monatize.it/legal/tos", address, nonce);
 
-    println!("{}", message);
     keccak256(
         format!(
             "{}{}{}",
